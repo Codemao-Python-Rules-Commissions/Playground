c2832be4542fa59665772a0e03427282af981de2
CoolPlayLin <CoolPlayLin@qq.com>
2023-10-14 15:01:20 +0800

    test

--- scripts/post.ts
+++ scripts/post.ts
@@ -185,7 +185,7 @@ export async function pull_request(
   switch (eventName) {
     case "pull_request_target":
       const result = await pull_request(
-        context.action,
+        context.workflow,
         context.repo.repo,
         context.repo.owner,
         context.payload.pull_request.number,
